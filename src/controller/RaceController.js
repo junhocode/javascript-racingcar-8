fe8aa3f533e507fd3ecd6c1e75016bef21fce82b
진준호 <161407999+junhocode@users.noreply.github.com>
2025-10-26 20:42:23 +0900

    fixed: Fixed Not using Parser

--- src/controller/RaceController.js
+++ src/controller/RaceController.js
@@ -1,30 +1,31 @@
 import InputView from '../view/inputView.js';
 import OutputView from '../view/outputView.js';
 import Car from '../service/models/Car.js';
+import CarParser from '../service/models/CarParser.js';
 import Race from '../service/models/Race.js';
 
 class RaceController {
   async run() {
     try {
       const carNamesInput = await InputView.getCars();
-      const carNames = carNamesInput.split(',');
-      const cars = carNames.map(name => Car.create(name.trim()));
+      const carNames = CarParser.parseNamesByComma(carNamesInput);
+      const cars = carNames.map(name => Car.create(name));
 
       const tryCountInput = await InputView.getTryCount();
       const tryCount = Number(tryCountInput);
 
       const race = Race.create(cars, tryCount);
 
+      OutputView.printResultHeader();
       race.startRace();
 
-      const allRoundResults = race.getResults();
-      OutputView.printRaceResult(allRoundResults);
+      const allRoundResults = race.formatResults();
+      OutputView.printAllRoundResults(allRoundResults);
 
-      const winners = race.getWinners();
+      const winners = race.findWinners();
       OutputView.printWinners(winners);
       
     } catch (error) {
-      OutputView.printError(error);
       throw error;
     }
   }
