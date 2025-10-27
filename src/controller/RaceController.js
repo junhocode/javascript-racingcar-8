b11e516ee952ae87d5b564bbc2977511978c8241
진준호 <161407999+junhocode@users.noreply.github.com>
2025-10-27 14:15:58 +0900

    fix: Fix Not catching NaN in RaceController

--- src/controller/RaceController.js
+++ src/controller/RaceController.js
@@ -3,6 +3,7 @@ import OutputView from '../view/outputView.js';
 import Car from '../service/models/Car.js';
 import CarParser from '../service/models/CarParser.js';
 import Race from '../service/models/Race.js';
+import { ERROR_MESSAGES } from '../constants/messages.js';
 
 class RaceController {
   async run() {
@@ -12,7 +13,11 @@ class RaceController {
       const cars = carNames.map(name => Car.create(name));
 
       const tryCountInput = await InputView.getTryCount();
+
       const tryCount = Number(tryCountInput);
+      if (Number.isNaN(tryCount)) {
+        throw new Error(ERROR_MESSAGES.TRYCOUNT_IS_NAN);
+      }
 
       const race = Race.create(cars, tryCount);
 
